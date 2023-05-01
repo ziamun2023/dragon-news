5178920669ff5ce239605595215cb312aa8a9413
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-05-01 12:41:07 +0600

    dragon server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express =require('express');
+const app=express();
+const cors=require('cors')
+const port =process.env.PORT || 5000;
+const categories=require('./data/categories.json')
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('dragon is running')
+
+})
+app.get('/categories',(req,res)=>{
+    // console.log(categories)
+    res.send(categories)
+})
+
+app.listen(port,()=>{
+    console.log(`dragon api is running port : ${port}`)
+})
